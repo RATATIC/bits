2c746223acc43f44fb3e6670f5b0b1dd68677567
Renat <kagal@itspartner.net>
2021-10-06 17:03:39 +0300

    story 1 : 1-10

---
+++ head.h
@@ -0,0 +1,24 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void task_1 (int n, FILE* fp);
+
+void task_2 (int n, FILE* fp);
+
+void task_3 (int n, FILE* fp);
+
+void task_4 (int n, FILE* fp);
+
+void task_5 (int n, int i, FILE* fp);
+
+void task_6 (int n, int i, FILE* fp);
+
+void task_7 (int n, int i, FILE* fp);
+
+void task_8 (int n, FILE* fp);
+
+void task_9 (int n, FILE* fp);
+
+void task_10 (int n, FILE* fp);
